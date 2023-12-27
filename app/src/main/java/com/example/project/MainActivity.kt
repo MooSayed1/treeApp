e69d839df541f6a8a708e29367d4b4c936982e18
Moosayed1 <mohamed1212213183@gmail.com>
2023-12-27 20:13:35 +0200

    login screen and singup

---
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.project
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var user = findViewById<TextInputEditText>(R.id.user)
+        var password = findViewById<TextInputEditText>(R.id.password)
+        val txt =findViewById<TextView>(R.id.textView)
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            if (user.text!!.isBlank()||user.length()<4) {
+                user.error = "User Name not less than 4 letters"
+            }
+
+            if (password.text!!.isBlank()) {
+                password.error = "please enter valid password"
+            }
+        }
+
+
+
+
+        txt.setOnClickListener {
+            startActivity(Intent(this,singUp::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/project/singUp.kt
@@ -0,0 +1,36 @@
+package com.example.project
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+
+class singUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+
+        var user = findViewById<TextInputEditText>(R.id.username)
+        var password = findViewById<TextInputEditText>(R.id.password)
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            if (user.text!!.isBlank()) {
+                user.error = "User Name not less than 4 letters"
+            }
+            var email =  findViewById<TextInputEditText>(R.id.email)
+            if(email.text!!.isBlank()){
+               email.error ="plead enter a valid email"
+           }
+            if (password.text!!.isBlank()) {
+                password.error = "please enter valid password"
+            }
+        }
+
+        val txt =findViewById<TextView>(R.id.textView)
+        txt.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
+}
